3a5593fbc068a4c657ddf2928e70adbeda5138a0
yhc999 <2211824688@qq.com>
2025-04-27 19:46:11 +0800

    first commit

---
+++ parsexec.c
@@ -0,0 +1,28 @@
+i#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+bool parseAndExecute(char *input)
+{
+   char *verb = strtok(input, " \n");
+   char *noun = strtok(NULL, " \n");
+   if (verb != NULL)
+   {
+      if (strcmp(verb, "quit") == 0)
+      {
+         return false;
+      }
+      else if (strcmp(verb, "look") == 0)
+      {
+         printf("It is very dark in here.\n");
+      }
+      else if (strcmp(verb, "go") == 0)
+      {
+         printf("It's too dark to go anywhere.\n");
+      }
+      else
+      {
+         printf("I don't know how to '%s'.\n", verb);
+      }
+   }
+   return true;
+}
